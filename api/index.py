89dcd6aae094913e112314cf64791d835a63c1f8
Rohan Sadhukhan <68492239+rohansadhukhan65@users.noreply.github.com>
2025-01-08 01:07:46 +0530

    init

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_socketio import SocketIO , emit
 
+# Initialize Flask app and SocketIO
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
@@ -9,3 +12,8 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+
+@socketio.on('connect')
+def handle_connect():
+    print("🔌 Socket Connected 🟢")
